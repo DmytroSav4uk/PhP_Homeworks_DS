0f399eb9926104fd73ab5fcb548856f7cb9b7ecb
Dmytro Savchuk <dmytro.savchuk.ua@gmail.com>
2025-02-20 16:11:03 +0200

    fixed form error.

--- templates/pages/3.php
+++ templates/pages/3.php
@@ -24,48 +24,51 @@ $language = $_COOKIE['lang'] ?? "ua";
     <a href="?<?= http_build_query(array_merge($_GET, ['lang' => 'ua'])) ?>">
         <svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-ua" viewBox="0 0 640 480" width="40px" height="40px">
             <g fill-rule="evenodd" stroke-width="1pt">
-                <path fill="gold" d="M0 0h640v480H0z" />
-                <path fill="#0057b8" d="M0 0h640v240H0z" />
+                <path fill="gold" d="M0 0h640v480H0z"/>
+                <path fill="#0057b8" d="M0 0h640v240H0z"/>
             </g>
         </svg>
     </a>
     <a href="?<?= http_build_query(array_merge($_GET, ['lang' => 'eng'])) ?>">
         <svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-us" viewBox="0 0 640 480" width="40px" height="40px">
-            <path fill="#bd3d44" d="M0 0h640v480H0" />
-            <path stroke="#fff" stroke-width="37" d="M0 55.3h640M0 129h640M0 203h640M0 277h640M0 351h640M0 425h640" />
-            <path fill="#192f5d" d="M0 0h364.8v258.5H0" />
+            <path fill="#bd3d44" d="M0 0h640v480H0"/>
+            <path stroke="#fff" stroke-width="37" d="M0 55.3h640M0 129h640M0 203h640M0 277h640M0 351h640M0 425h640"/>
+            <path fill="#192f5d" d="M0 0h364.8v258.5H0"/>
             <marker id="us-a" markerHeight="30" markerWidth="30">
-                <path fill="#fff" d="m14 0 9 27L0 10h28L5 27z" />
+                <path fill="#fff" d="m14 0 9 27L0 10h28L5 27z"/>
             </marker>
-            <path fill="none" marker-mid="url(#us-a)" d="m0 0 16 11h61 61 61 61 60L47 37h61 61 60 61L16 63h61 61 61 61 60L47 89h61 61 60 61L16 115h61 61 61 61 60L47 141h61 61 60 61L16 166h61 61 61 61 60L47 192h61 61 60 61L16 218h61 61 61 61 60z" />
+            <path fill="none" marker-mid="url(#us-a)"
+                  d="m0 0 16 11h61 61 61 61 60L47 37h61 61 60 61L16 63h61 61 61 61 60L47 89h61 61 60 61L16 115h61 61 61 61 60L47 141h61 61 60 61L16 166h61 61 61 61 60L47 192h61 61 60 61L16 218h61 61 61 61 60z"/>
         </svg>
     </a>
     <a href="?<?= http_build_query(array_merge($_GET, ['lang' => 'pl'])) ?>">
         <svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-pl" viewBox="0 0 640 480" width="40px" height="40px">
             <g fill-rule="evenodd">
-                <path fill="#fff" d="M640 480H0V0h640z" />
-                <path fill="#dc143c" d="M640 480H0V240h640z" />
+                <path fill="#fff" d="M640 480H0V0h640z"/>
+                <path fill="#dc143c" d="M640 480H0V240h640z"/>
             </g>
         </svg>
     </a>
     <a href="?<?= http_build_query(array_merge($_GET, ['lang' => 'de'])) ?>">
         <svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-de" viewBox="0 0 640 480" width="40px" height="40px">
-            <path fill="#fc0" d="M0 320h640v160H0z" />
-            <path fill="#000001" d="M0 0h640v160H0z" />
-            <path fill="red" d="M0 160h640v160H0z" />
+            <path fill="#fc0" d="M0 320h640v160H0z"/>
+            <path fill="#000001" d="M0 0h640v160H0z"/>
+            <path fill="red" d="M0 160h640v160H0z"/>
         </svg>
     </a>
     <a href="?<?= http_build_query(array_merge($_GET, ['lang' => 'fr'])) ?>">
         <svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-fr" viewBox="0 0 640 480" width="40px" height="40px">
-            <path fill="#fff" d="M0 0h640v480H0z" />
-            <path fill="#000091" d="M0 0h213.3v480H0z" />
-            <path fill="#e1000f" d="M426.7 0H640v480H426.7z" />
+            <path fill="#fff" d="M0 0h640v480H0z"/>
+            <path fill="#000091" d="M0 0h213.3v480H0z"/>
+            <path fill="#e1000f" d="M426.7 0H640v480H426.7z"/>
         </svg>
     </a>
 </div>
 <div>
     Вибрана мова: <?= $languages[$language] ?>
 </div>
+
+
 <form action="./index.php" method="post" enctype="multipart/form-data">
 
     <table>
@@ -81,40 +84,43 @@ $language = $_COOKIE['lang'] ?? "ua";
             <td>Пароль (ще раз):</td>
             <td><input type="password" name="passwordRepeat" value=<?= $passwordRepeat ?? "" ?>></td>
         </tr>
-        <tr>
+
+        <tr style="display: flex">
             <td>Стать:</td>
-            <td>
+            <td style="display: flex;justify-content: center">
                 <?php foreach ($genders as $key => $gender): ?>
-                    <input type="radio" value="<?= $gender ?>" id="<?= $key . $gender ?>" name="gender" <?= $chosenGender === $gender ? 'checked' : '' ?>>
-                    <label for="<?= $key . $gender ?>"><?= $gender ?></label>
+                    <div style="display: flex;">
+                        <div>
+                            <label for="gender_<?= $key ?>"><?= $gender ?></label>
+                            <input style="margin: 0;width: 50%" type="radio" value="<?= $gender ?>"
+                                   id="gender_<?= $key ?>"
+                                   name="gender" <?= $chosenGender === $gender ? 'checked' : '' ?>>
+                        </div>
+                    </div>
+
                 <?php endforeach ?>
-            </td>
-        </tr>
-        <tr>
-            <td>Місто:</td>
-            <td>
-                <select name="city" id="city">
-                    <?php foreach ($cities as $city): ?>
-                        <option value="<?= $city ?>" <?= $chosenCity === $city ? 'selected' : '' ?>><?= $city ?></option>
-                    <?php endforeach ?>
-                </select>
             </td>
         </tr>
         <tr>
             <td>Улюблені ігри:</td>
-            <td>
-                <?php foreach ($favGames as $game): ?>
-                    <input type="checkbox" name="favGames[]" id="<?= $game ?>" value="<?= $game ?>" <?= in_array($game, $chosenGames) ? 'checked' : '' ?>>
-                    <label for="<?= $game ?>"><?= $game ?></label>
+            <td style="display: flex;margin-bottom: 10px;margin-top: 10px ">
+                <?php foreach ($favGames as $key => $game): ?>
+                <div>
+                    <input type="checkbox" name="favGames[]" id="game_<?= $key ?>"
+                           value="<?= $game ?>" <?= in_array($game, $chosenGames) ? 'checked' : '' ?>>
+                    <label for="game_ <?= $key ?>"><?= $game ?> </label>
+                </div>
+
                 <?php endforeach ?>
             </td>
         </tr>
+
         <tr>
             <td>Про себе:</td>
             <td><textarea name="about" id="" value=<?= $about ?? "" ?>></textarea></td>
         </tr>
         <tr>
-            <td>Фотографія:</td>2
+            <td>Фотографія:</td>
             <td><input type="file" name="userPhoto" id="userPhoto"></td>
         </tr>
         <tr>
